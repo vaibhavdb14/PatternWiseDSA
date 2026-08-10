82f1cd89b2c7d5a54d0cec89667f0bd86e2f68ea
Vaibhav Bodade <vdbodade140705@gmail.com>
2026-08-10 22:09:24 +0530

    feat: solved - Counting Inversion In Array

---
+++ AskedProblems/CountingInversionInArray.java
@@ -0,0 +1,30 @@
+/*
+    Problem: Counting Inversions in an Array
+    Given an array of integers, count the number of inversions in the array.
+    An inversion is a pair of indices (i, j) such that i < j and arr[i] > arr[j].
+
+*/
+
+public class CountingInversionInArray {
+    
+    public static int countInversions(int[] arr) {
+        int n = arr.length;
+        int inversions = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    inversions++;
+                }
+            }
+        }
+
+        return inversions;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3, 5};
+        int inversionCount = countInversions(arr);
+        System.out.println("Number of inversions in the array: " + inversionCount);
+    }
+}
