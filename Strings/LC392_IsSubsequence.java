fce2e9594439286e6da87410c69ae309d9102638
Vaibhav Bodade <vdbodade140705@gmail.com>
2026-08-18 00:37:21 +0530

    feat: Solved LC392 - Is Subsequence

---
+++ Strings/LC392_IsSubsequence.java
@@ -0,0 +1,43 @@
+class LC392_IsSubsequence {
+    public static boolean isSubsequence(String s, String t) {
+        int k = 0;
+
+        for(int i = 0; i < t.length(); i++){
+            if(k < s.length() && s.charAt(k) == t.charAt(i)){
+                k++;
+            }
+        }
+
+        if(k == s.length()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+
+        if(isSubsequence(s, t)){
+            System.out.println("Yes, Subsequence");
+        }else{
+            System.out.println("No, Subsequence");
+        }
+
+        s = "axc";
+        t = "ahbgdc";
+
+        if(isSubsequence(s, t)){
+            System.out.println("Yes, Subsequence");
+        }else{
+            System.out.println("No, Subsequence");
+        }
+    }
+}
+
+/**
+ * link : https://leetcode.com/problems/is-subsequence/description/
+ * Time - O(n)
+ * Space - O(1)
+ */
